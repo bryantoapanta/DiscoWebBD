08e323e9bef7354f2b80e32b90d6dc0bcfa7d939
bryantoapanta <35955907+bryantoapanta@users.noreply.github.com>
2020-02-16 01:08:34 +0100

    Add files via upload

---
+++ app/controlerFile.php
@@ -0,0 +1,208 @@
+<?php
+include_once 'config.php';
+include_once 'modeloUserDB.php';
+
+// include_once 'modeloUser.php';
+function ctlFileVerFicheros($msg)
+{
+    $ficheros = modeloUserGetFiles(); // almaceno dentro de $usuarios el contenido de la sesion usuarios
+                                      // Invoco la vista
+    include_once 'plantilla/verarchivos.php';
+}
+
+function ctlFileNuevo($msg)
+{
+    // se incluyen los códigos de error que produce la subida de archivos en PHPP
+    // Posibles errores de subida
+    $codigosErrorSubida = [
+        0 => 'Subida correcta',
+        1 => 'El tamaño del archivo excede el admitido por el servidor', // directiva upload_max_filesize en php.ini
+        2 => 'El tamaño del archivo excede el admitido por el cliente', // directiva MAX_FILE_SIZE en el formulario HTML
+        3 => 'El archivo no se pudo subir completamente',
+        4 => 'No se seleccionó ningún archivo para ser subido',
+        6 => 'No existe un directorio temporal donde subir el archivo',
+        7 => 'No se pudo guardar el archivo en disco', // permisos
+        8 => 'Una extensión PHP evito la subida del archivo' // extensión PHP
+    ];
+    $msg = '';
+
+    // si no se reciben el archivo, se se carga la pagina para subir el archivo
+    if ((! isset($_FILES['archivo1']['name']))) {
+        include_once 'plantilla/subirfichero.php';
+    } else { // se reciben el directorio de alojamiento y el archivo
+             // $directorioSubida = RUTA_UBUNTU . $_SESSION["user"] . "/"; // $_session user para crear una carpetadel usuario
+             // directorioubuntu = "/home/alummo2019-20/Escritorio/prueba/" . $_SESSION["user"] . "/";
+        $directorioSubida = RUTA_FICHEROS . "/" . $_SESSION["user"] . "/";
+        if (! (file_exists($directorioSubida))) {
+            // mkdir($directorioSubida, 0777, true);
+            mkdir($directorioSubida, 0777, true);
+        }
+        // debe permitir la escritua para Apache
+        //echo $directorioSubida; // Información sobre el archivo subido
+        $nombreFichero = $_FILES['archivo1']['name'];
+        $tipoFichero = $_FILES['archivo1']['type'];
+        $tamanioFichero = $_FILES['archivo1']['size'];
+        $temporalFichero = $_FILES['archivo1']['tmp_name'];
+        $errorFichero = $_FILES['archivo1']['error'];
+
+        // CREO UN ARRAY DONDE ALMACENAR LOS DATOS DEL FICHERO
+        $id = $_SESSION["user"];
+        $plan = ModeloUserDB::ObtenerTipo($id);
+        echo $plan;
+        $data = [
+            $nombreFichero,
+            $directorioSubida,
+            $tipoFichero,
+            $tamanioFichero
+            // $temporalFichero,
+        ];
+
+        // PRIMERO SUBO EL FICHERO Y LUEGO SI SE SUBE ALMACENO LOS DATOS EN EL JSON
+        if (cumpleEspacio($id,$plan,$tamanioFichero)) {
+            if (modelouserSubirfichero($directorioSubida, $nombreFichero, $temporalFichero, $errorFichero, $msg)) {
+                if (modeloficheroAdd($id, $data, $nombreFichero)) {
+                    $msg .= "<br>Exito al almacenar datos";
+                }
+            }
+        }else{
+            $msg .= "No se ha podido subir, ha llegado al espacio l�mite especificado para su plan<br>";
+        }
+    }
+    modeloUserSave();
+    ctlFileVerFicheros($msg);
+}
+
+function ctlFileBorrar($msg)
+{
+    $msg = "";
+    $user = $_GET['id'];
+    $nombre = $_GET['nombre'];
+    $directorioSubida = RUTA_FICHEROS . "/" . $_SESSION["user"] . "/" . $nombre;
+    echo $user;
+    if (unlink($directorioSubida)) {
+        modeloUserDelfichero($user);
+        
+          $msg = "El archivo se borró correctamente.";
+        
+    } else {
+        $msg = "No se pudo borrar el archivo.";
+    }
+    modeloUserSave();
+    ctlFileVerFicheros($msg);
+}
+
+function ctlFileRenombrar($msg)
+{
+    $msg = "";
+    $nombrefich = $_GET['id'];
+    $nombre = $_GET['nombre'];
+    $nuevoNombre = $_GET['nuevo'];
+    echo $nuevoNombre;
+    echo $nombrefich;
+    echo $nombre;
+
+    if (modeloUserRenamefichero($nombrefich, $nuevoNombre)) {
+        // $nombreAntiguo = "/home/alummo2019-20/Escritorio/prueba/".$_SESSION["user"]."/".$nombre;
+        // $nombreNuevo = "/home/alummo2019-20/Escritorio/prueba/".$_SESSION["user"]."/".$nuevoNombre;
+        $nombreAntiguo = RUTA_FICHEROS . "/" . $_SESSION["user"] . "/" . $nombre;
+        $nombreNuevo = RUTA_FICHEROS . "/" . $_SESSION["user"] . "/" . $nuevoNombre;
+
+        echo $nombreAntiguo . "---->" . $nombreNuevo;
+        // RENOMBRAR ARCHIVO
+        rename($nombreAntiguo, $nombreNuevo);
+
+        $msg = "El archivo se ha sido modificado correctamente.";
+    } else {
+        $msg = "No se pudo modificar el archivo.";
+    }
+    modeloUserSave();
+    ctlFileVerFicheros($msg);
+}
+
+function ctlFileCompartir($msg)
+{
+    $usuarios = modeloUserGetAll(); // almaceno dentro de $usuarios el contenido de la sesion usuarios
+                                    // Invoco la vista
+    include_once 'plantilla/verarchivos.php';
+}
+
+function ctlFileUserCerrar($msg)
+{
+    session_destroy();
+    modeloUserSave();
+    header('Location:index.php');
+}
+
+function ctlFileDescargar($msg)
+{
+    $nombre_fichero = $_GET["id"];
+    echo $nombre_fichero;
+    // $datosfichero = $_SESSION["ficheros"][$_SESSION["user"]][$nombre_fichero];
+    // $nombre = $datosusuario[0];
+    $directorio = RUTA_FICHEROS . "/" . $_SESSION["user"];
+    echo "<br>" . $directorio;
+
+    modelouserDescargar($nombre_fichero, $directorio, $msg);
+    ctlFileVerFicheros($msg);
+}
+
+function ctlFileModificar()
+{
+    $msg = "";
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        if (isset($_POST['clave1']) && isset($_POST['email']) && isset($_POST['nombre']) && isset($_POST['plan'])) {
+            $id = $_POST['iduser'];
+            $nombre = $_POST['nombre'];
+            $clave = $_POST['clave1'];
+            $mail = $_POST['email'];
+            $plan = $_POST['plan'];
+
+            // Si el plan se modifica entonces el estado pasa a BLOQUEADO
+            // ECHO $plan . " plan antiguo: " . $plan2;
+            $datosusuario = modelouserdb::GetAllModificar($id);
+            $planAntiguo = $datosusuario[4];
+            echo $planAntiguo;
+            if ($plan != $planAntiguo) {
+                $estado = "B";
+            }
+            echo $estado;
+            // CREO UN ARRAY DONDE ALMACENAR LA INFORMACION PARA LUEGO PASARLO COMO PARAMETRO
+            $data = [
+                $clave,
+                $nombre,
+                $mail,
+                $plan,
+                $estado
+            ];
+
+            // SI NO HAY ERRORES
+            if (! ModeloUserDB::errorValoresModificar($id, $data[0], $_POST["clave2"], $data[1], $data[2], $data[3], $data[4])) {
+                $data[0] = Cifrador::cifrar($data[0]);
+                if (ModeloUserDB::UserUpdate($id, $data)) {
+                    $msg = "El usuario fue modificado con éxito";
+                } else {
+                    $msg = "El usuario no pudo ser modificado";
+                }
+            } else {
+                $msg = "El usuario no pudo ser modificado";
+            }
+        }
+    } else {
+
+        // al pulsar en modificar le paso el id, con ese id sacamos los datos del id(usuario) para, que luego se mostraran a la hora de modificar
+        $user = $_SESSION["user"];
+        $datosusuario = modelouserdb::GetAllModificar($user);
+
+        $clave = $datosusuario[0];
+        $nombre = $datosusuario[1];
+        $mail = $datosusuario[2];
+        $plan = $datosusuario[3];
+        $estado = $datosusuario[4];
+        include_once 'plantilla/modificarficheros.php';
+    }
+    modeloUserSave();
+    ctlFileVerFicheros($msg);
+}
+
+?>
---
+++ app/modeloUser.php
@@ -0,0 +1,200 @@
+<?php
+include_once 'config.php';
+include_once 'modeloUserDB.php';
+/*
+ * DATOS DE USUARIO
+ * • Identificador ( 5 a 10 caracteres, no debe existir previamente, solo letras y números)
+ * • Contraseña ( 8 a 15 caracteres, debe ser segura)
+ * • Nombre ( Nombre y apellidos del usuario
+ * • Correo electrónico ( Valor válido de dirección correo, no debe existir previamente)
+ * • Tipo de Plan (0-Básico |1-Profesional |2- Premium| 3- Máster)
+ * • Estado: (A-Activo | B-Bloqueado |I-Inactivo )
+ */
+// Inicializo el modelo
+// Cargo los datos del fichero a la session
+function modeloUserInit()
+{
+   /* if (! isset($_SESSION['tusuarios'])) {
+        $datosjson = @file_get_contents(FILEUSER) or die("ERROR al abrir fichero de usuarios");
+        $tusuarios = json_decode($datosjson, true);
+        $_SESSION['tusuarios'] = $tusuarios;
+    }*/
+
+    // SI LA SESION FICHEROS NO EXISTE , CREO UNA Y CARGO LOS DATOS
+    if (! isset($_SESSION['ficheros'])) {
+        $datosjson = @file_get_contents(FILE) or die("ERROR al abrir fichero de datos");
+        $ficheros = json_decode($datosjson, true);
+        $_SESSION['ficheros'] = $ficheros;
+    }
+}
+
+
+
+
+// Vuelca los datos al fichero
+function modeloUserSave()
+{
+    // fclose($fich);
+    // GUARDAR DATOS FICHEROS
+    $datosficherojon = json_encode($_SESSION['ficheros']);
+    file_put_contents(FILE, $datosficherojon) or die("Error al escribir en el fichero.");
+}
+
+
+
+// MODELO USER FICHEROS--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function modeloUserGetFiles()
+{
+    $vista = [];
+    // REALIZAMOS UN FOR-EACH PARA SACAR LOS DATOS DEL USUARIO CONECTADO. EL USUARIO LO SACAMOS A ATRAVES DE $_SESSION["USER"]
+    foreach ($_SESSION['ficheros'] as $usuario => $nombrefich) {
+
+        if ($usuario == $_SESSION["user"]) { // si el usuario coincide con el usuario de la sesion
+
+            foreach ($_SESSION['ficheros'][$_SESSION["user"]] as $nombrefich => $datos) { // realizo un for each para recorrer la tabla de archivos con sus respectivos datos.
+                echo "<br>" . $nombrefich;
+
+                $vista[$nombrefich] = [
+                    $datos[0],
+                    $datos[1],
+                    $datos[2],
+                    $datos[3]
+                    // $datosusuario[4]
+                ];
+            }
+        }
+    }
+    return $vista;
+}
+
+// A�ADIR DATOS DE FICHERO
+function modeloficheroAdd($userid, $userdat, $nombrefichero)
+{
+    $_SESSION["ficheros"][$userid][$nombrefichero] = $userdat;
+    return true;
+}
+
+// SUBIR ARCHIVO A LA "NUBE"
+function modelouserSubirfichero($directorioSubida, $nombreFichero, $temporalFichero, $errorFichero, &$msg)
+{
+    // Obtengo el código de error de la operación, 0 si todo ha ido bien
+    if ($errorFichero > 0) {
+        $msg .= "Se a producido el error: $errorFichero:" . $codigosErrorSubida[$errorFichero] . ' <br />';
+        return false;
+    } else { // subida correcta del temporal
+             // si es un directorio y tengo permisos
+        if (is_dir($directorioSubida) && is_writable($directorioSubida)) {
+            // Intento mover el archivo temporal al directorio indicado
+            if (move_uploaded_file($temporalFichero, $directorioSubida . '/' . $nombreFichero) == true) {
+                // $msg .= 'Archivo guardado en: ' . $directorioSubida .'/'. $nombreFichero . ' <br />';
+                $msg = "Archivo guardado con exito";
+                return true;
+            } else {
+                $msg .= 'ERROR: Archivo no guardado correctamente <br />';
+                return false;
+            }
+        } else {
+            $msg .= 'ERROR: No es un directorio correcto o no se tiene permiso de escritura <br />';
+            return FALSE;
+        }
+    }
+}
+
+function calcular_Espacio($userid){
+    $suma=0;
+    
+    echo $userid;
+    foreach ($_SESSION['ficheros'] as $usuario => $nombrefich) {
+        
+        if ($usuario == $userid) { // si el usuario coincide con el usuario de la sesion
+            
+            foreach ($_SESSION['ficheros'][$userid] as $nombrefich => $datos) { // realizo un for each para recorrer la tabla de archivos con sus respectivos datos.
+                echo "<br>" . $nombrefich;
+                
+                $suma=$suma+$datos[3];//sumo el tama�o de cada archivo
+                echo $suma;
+            }
+        }
+    }
+    return $suma;
+}
+
+function cumpleEspacio($user, $plan, $tamanho){
+    $espacio=calcular_Espacio($user);
+    
+    switch ($plan) {
+        case "B�sico":
+            //Asignamos 2MB de espacio para el plan B�sico
+            if(($tamanho + $espacio)< 2000000 ) return true;
+            break;
+        case "Profesional":
+            //Asignamos 5MB de espacio para el plan Profesional
+            if(($tamanho + $espacio)< 5000000 ) return true;
+            break;
+        case "Premium":
+            //Asignamos 10MB de espacio para el plan Premium
+            if(($tamanho + $espacio)< 10000000 ) return true;
+            break;
+    }
+    return false;
+}
+
+// DESCARGAR FICHERO
+function modelouserDescargar($nombrefichero, $directorio, &$msg)
+{
+    // OBTENGO EL NOMBRE DEL FICHERO
+    $fileName = basename($nombrefichero);
+    // OBTENGO LA RUTA COMPLETA DEL FICHERO
+    $filePath = $directorio . "/" . $fileName;
+    echo $filePath;
+    // SI NO ESTA VACIO Y EXISTE LA RUTA
+    if (! empty($fileName) && file_exists($filePath)) {
+        // Define headers
+        header("Cache-Control: public");
+        header("Content-Description: File Transfer");
+        header("Content-Disposition: attachment; filename=$fileName");
+        header("Content-Type: application/zip");
+        header("Content-Transfer-Encoding: binary");
+
+        // Read the file
+        readfile($filePath);
+        $msg = "Archivo descargado";
+        exit();
+    } else {
+        $msg = 'The file does not exist.';
+    }
+}
+
+// BORRAR DATOS FICHERO
+FUNCTION modeloUserDelfichero($fichero)
+{
+    $borrado = false;
+   
+    foreach ($_SESSION["ficheros"][$_SESSION["user"]] as $nFichero => $valor) { // recorremos el array en busca del usuario
+
+        if ($fichero == $nFichero) {
+            //foreach ($_SESSION["ficheros"][$_SESSION["user"]][$clave] as $nombrefich => $valor) { // borramos el archivo y sus datos
+                unset($_SESSION["ficheros"][$_SESSION["user"]][$fichero]);
+                array_values($_SESSION["ficheros"][$_SESSION["user"]]);
+                $borrado = true;
+           // }
+        }
+    }
+    return $borrado;
+}
+
+// RENOMBRAR ARCHIVO
+FUNCTION modeloUserRenamefichero($antiguo, $nuevo)
+{
+    $rename = false;
+    foreach ($_SESSION["ficheros"][$_SESSION["user"]] as $clave => $valor) {
+        if ($clave == $antiguo) {
+            // SI SE ENCUENTRA EL USUARIO, CAMBIAMOS EL NOMBRE DEL ARCHIVO POR EL NUEVO VALOR.
+            $_SESSION["ficheros"][$_SESSION["user"]][$clave][0] = $nuevo;
+          
+            
+            $rename = true;
+        }
+    }
+    return $rename;
+}
